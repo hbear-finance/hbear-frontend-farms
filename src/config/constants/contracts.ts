59d0b7c0beb706f6fd6665f43d198b8b9f99055d
Erdem <hbearstake@yandex.com>
2021-03-31 21:46:14 +0300

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0x2c573649Dd2A19Bd21ccC9634A6cBc141926b048',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x22A34e17D5C35e872A30664BB143F3FEB11a98e4',
     97: '',
   },
   wbnb: {
