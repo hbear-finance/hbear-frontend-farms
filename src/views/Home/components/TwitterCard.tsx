d34aef7746747cd265bfe8331f5b506bc3e613d2
Erdem <hbearstake@yandex.com>
2021-04-02 14:00:13 +0300

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'yBEARSwap'
+            screenName: 'YieldBear'
           }}
           options={{
             height: '300',
