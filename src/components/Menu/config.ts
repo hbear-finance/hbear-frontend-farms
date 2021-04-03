f01fab121b3f818b75e0192c410643c955baf943
Erdem <hbearstake@yandex.com>
2021-04-03 23:44:30 +0300

    -

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,11 +12,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.goosedefi.com/',
+        href: 'https://exchange.sbeardemo.cf/',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.goosedefi.com/#/pool',
+        href: 'https://exchange.sbeardemo.cf/#/pool',
       },
     ],
   },
