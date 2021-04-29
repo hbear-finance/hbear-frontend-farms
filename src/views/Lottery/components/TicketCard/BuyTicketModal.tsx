e948f0610c36283bc875af65bc390364cfe013a1
Erdem <hbearstake@yandex.com>
2021-04-30 00:27:39 +0300

    -

--- src/views/Lottery/components/TicketCard/BuyTicketModal.tsx
+++ src/views/Lottery/components/TicketCard/BuyTicketModal.tsx
@@ -72,21 +72,21 @@ const BuyTicketModal: React.FC<BuyTicketModalProps> = ({ max, onDismiss }) => {
         onSelectMax={handleSelectMax}
         onChange={handleChange}
         max={fullBalance}
-        symbol="TICKET"
-        availableSymbol="CAKE"
+        symbol="SBT"
+        availableSymbol="sbear"
       />
       <div>
-        <Tips>{TranslateString(456, 'Your amount must be a multiple of 10 CAKE')}</Tips>
-        <Tips>{TranslateString(458, '1 Ticket = 10 CAKE')}</Tips>
+        <Tips>{TranslateString(456, 'Your amount must be a multiple of 10 sBEAR')}</Tips>
+        <Tips>{TranslateString(458, '1 Ticket = 10 sBEAR')}</Tips>
       </div>
       <div>
         <Announce>
           {TranslateString(
             478,
-            'Ticket purchases are final. Your CAKE cannot be returned to you after buying tickets.',
+            'Ticket purchases are final. Your sBEAR cannot be returned to you after buying tickets.',
           )}
         </Announce>
-        <Final>{TranslateString(460, `You will spend: ${cakeCosts(val)} CAKE`)}</Final>
+        <Final>{TranslateString(460, `You will spend: ${cakeCosts(val)} sBEAR`)}</Final>
       </div>
       <ModalActions>
         <Button fullWidth variant="secondary" onClick={onDismiss}>
