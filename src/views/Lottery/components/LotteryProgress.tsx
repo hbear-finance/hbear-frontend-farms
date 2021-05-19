17c950bd58d0fc7aaa6d2d64877ae467858bb37a
sbear-finance <81658401+sbear-finance@users.noreply.github.com>
2021-05-19 11:30:51 +0300

    Update LotteryProgress.tsx

--- src/views/Lottery/components/LotteryProgress.tsx
+++ src/views/Lottery/components/LotteryProgress.tsx
@@ -43,12 +43,22 @@ const LotteryProgress = () => {
 
   return (
     <ProgressWrapper>
-	<Progress primaryStep={getLotteryDrawStep(currentMillis)} secondaryStep={getTicketSaleStep()} showProgressBunny />
+      <Progress primaryStep={getLotteryDrawStep(currentMillis)} secondaryStep={getTicketSaleStep()} showProgressBunny />
+      <TopTextWrapper>
+        <StyledPrimaryText fontSize="20px" bold color="contrast">
+          {lotteryHasDrawn ? timeUntilTicketSale : timeUntilLotteryDraw}
+        </StyledPrimaryText>
+        <Text fontSize="20px" bold color="invertedContrast">
+          {lotteryHasDrawn ? TranslateString(0, 'Until ticket sale') : TranslateString(0, 'Until lottery draw')}
+        </Text>
+      </TopTextWrapper>
+      {lotteryHasDrawn && (
         <BottomTextWrapper>
-          <Text fontSize="20px" bold color="contrast">
-            {TranslateString(0, 'Upcoming Draw :  13/5/2021 16:00:00 UTC')}
+          <Text color="invertedContrast">
+            {timeUntilLotteryDraw} {TranslateString(0, 'Until lottery draw')}
           </Text>
         </BottomTextWrapper>
+      )}
     </ProgressWrapper>
   )
 }
